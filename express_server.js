bf3436e6419f508434fb5c1bf109baeb7cbd8a55
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-11 22:37:31 +0000

    created new template urls_new

--- express_server.js
+++ express_server.js
@@ -1,8 +1,16 @@
+const bodyParser = require("body-parser");
 const express = require('express')
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+    return Math.floor(Math.random() * 1000000)
+}
+
+
 
 const urlDatabase = {
     'lighthouse': 'http://www.lighthouselabs.ca',
@@ -18,6 +26,15 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+    res.render('urls_new')
+})
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);   
+    res.send('OK');          
+  });
+
 app.get('/urls/:shortURL', (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render('urls_show', templateVars);
@@ -30,6 +47,7 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
     res.send('<html><body>Hello <b>World</b></body></html>\n'); 
 })
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
